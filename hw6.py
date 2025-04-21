b970382b6c8b3e0c49eda3e2481bf722a7e80859
yoseop1 <yosube5257@gmail.com>
2025-04-21 13:08:57 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for n in range(1, 10):
+    for m in range(2,6):
+        print(f'{m} * {n} = {m*n:2d}',end='\t')
+    print()
+
+print()
+
+for n in range(1, 10):
+    for m in range(6,10):
+        print(f'{m} * {n} = {m*n:2d}',end='\t')
+    print()
